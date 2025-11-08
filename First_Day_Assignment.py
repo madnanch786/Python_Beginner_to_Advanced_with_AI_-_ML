9562127e8cfcdcd8c29523d4c052040cb0acd052
madnanch786 <madnanch2016@gmail.com>
2025-11-08 11:02:12 -0800

    ADDED: First Day Assignment File

---
+++ First_Day_Assignment.py
@@ -0,0 +1,21 @@
+''' Write a progra to print a statement 10 times
+Ans: There are Two ways to do this
+1. write print statement 10 time Like 
+print("Satement 1")
+print("Satement 2")
+print("Satement 3")
+.
+.
+.
+print("Satement 10")
+
+2. Print the statement by using loop statement
+
+'''
+stno=1
+for i in range(5):
+    print("Statemnt", stno)
+    stno=stno+1
+
+
+
